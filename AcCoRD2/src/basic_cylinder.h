a471f1ea84b84cc07bdd46e6c4a53aec304d329e
Jack Kirby <jackrekirby@gmail.com>
2021-03-09 18:13:29 +0000

    added licensing comments

--- AcCoRD2/src/basic_cylinder.h
+++ AcCoRD2/src/basic_cylinder.h
@@ -1,3 +1,7 @@
+// The AcCoRD 2 Simulator (Actor - based Communication via Reaction - Diffusion)
+// Copyright 2021 Jack Kirby. All rights reserved. 
+// For license details, read LICENSE.txt in the root AcCoRD2 directory
+
 #pragma once
 #include "vec3d.h"
 #include "vec2d.h"
